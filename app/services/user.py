0706ddca0666823a69541d25765d404045869769
onesch <vladimiravakov12345@gmail.com>
2026-04-18 13:58:51 +0400

    fix(users): improve create_user logic by removing 'with db.begin():' block

--- app/services/user.py
+++ app/services/user.py
@@ -7,21 +7,20 @@ from app.models.user import User
 
 def create_user(db: Session, username: str) -> User:
     """Add new user to db and check user in database."""
-    with db.begin():  # execution in one transaction, get and add user.
-        query = select(User).where(User.username == username)
-        user = db.execute(query).scalars().first()
+    user = db.execute(
+        select(User).where(User.username == username)
+    ).scalars().first()
 
-        if not user:
-            db_user = User(username=username)
+    if user:
+        raise HTTPException(
+            status_code=409,
+            detail="user name already taken."
+        )
 
-            db.add(db_user)
-            db.commit()
-            db.refresh(db_user)
+    db_user = User(username=username)
+    db.add(db_user)
 
-            return db_user
-        else:
-            detail = "user name already taken."
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail=detail,
-            )
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
